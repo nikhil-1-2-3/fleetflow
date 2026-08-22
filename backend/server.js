ee7017d76bebda7864197083b4c154958c7f7ba2
nikhil121-singh <singhn8446@gmail.com>
2026-08-22 21:04:47 +0530

    Fix CORS policy to allow K8s port 9090

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: ['http://localhost:5173', 'http://localhost:9090'], 
     credentials: true,
 }));
 app.use(helmet());
